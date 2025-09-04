f6425420208c7b8df188a6d3919777cb67be3f63
rabbie-wu <595909839@qq.com>
2025-09-04 22:01:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,15 +1,15 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
-let proxyIP = '';
-let DNS64Server = '';
-//let sub = '';
+let userID = 'rabbie';
+let proxyIP = 'rabbiewu';
+let DNS64Server = 'cfrabbie';
+//let sub = 'wurabbie';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
 let subProtocol = 'https';
 let subEmoji = 'true';
-let socks5Address = '';
+let socks5Address = 'wunengwen';
 let parsedSocks5Address = {};
 let enableSocks = false;
 let enableHttp = false;
@@ -4568,3 +4568,4 @@ async function nginx() {
 	`
     return text;
 }
+
